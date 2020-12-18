a6838f3afe73fdb3cfca88c3d1bc16f78556e84f
Delitas <76210621+Delitas@users.noreply.github.com>
2020-12-18 21:42:20 +0900

    Add files via upload

---
+++ convert_to_decimal(Use int func).py
@@ -0,0 +1,15 @@
+"""
+Data : 2020-12-18
+dev : Python 3.8
+Auth : Delitas
+"""
+print('Ver 1.0')
+print('Convert to Decimal')
+print('Author : Delitas')
+print('='*20)
+numbers = input('Input number : ')
+base = int(input("Input number's base (1<base<11) : "))
+covert_num = int(numbers,base)
+print(covert_num)
+
+
